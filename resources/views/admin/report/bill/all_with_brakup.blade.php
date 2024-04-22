cf6889a4462dfec59aa8b44856485e4d70f67fb6
zarifkhanafridi <afridiofkh@gmail.com>
2024-04-22 11:29:30 +0500

    update for breadkup report

--- resources/views/admin/report/bill/all_with_brakup.blade.php
+++ resources/views/admin/report/bill/all_with_brakup.blade.php
@@ -24,7 +24,7 @@ body {
 .header {
   width: 100%;
   float: left;
-  font-size: 2em;
+  font-size: 1.4em;
   text-align:center;
 }
 
@@ -168,16 +168,25 @@ a {
     <td class="headingTd ">E.cost </td>
     {{-- <td class="headingTd ">PAID FROM</td> --}}
     <td class="headingTd">Arear</td>
+    <td class="headingTd">Q.T Adjustment</td>
+    <td class="headingTd">F.S Surcharge</td>
+    <td class="headingTd">F.P.A</td>
+    <td class="headingTd">E.D</td>
+    <td class="headingTd">E.D on FPA	</td>
+    {{-- <td class="headingTd">	</td> --}}
     {{-- <td class="headingTd">c.bill</td> --}}
-   @foreach ($charges_types as $ctkey => $ctrow )
+   {{-- @foreach ($charges_types as $ctkey => $ctrow )
    <td class="headingTd">{{$ctrow->title}}</td>
-   @endforeach
+   @endforeach --}}
 
-   @foreach ($tax_types as $ttkey => $ttrow )
+   {{-- @foreach ($tax_types as $ttkey => $ttrow )
    <td class="headingTd">{{$ttrow->title}}</td>
-   @endforeach
+   @endforeach --}}
     {{-- <td class="headingTd">Amount </td> --}}
     {{-- <td class="headingTd">l.p.surchage</td> --}}
+    <td class="headingTd"> S.Charg</td>
+    <td class="headingTd"> L.P.Charg</td>
+    <td class="headingTd"> Adjustment</td>
     <td class="headingTd">Amount after Due Date </td>
   </tr>
   <?php $c=1; 
@@ -203,26 +212,60 @@ a {
     {{-- <td class=""> {{$row->offpeak_units}}</td> --}}
     
     <td class="">{{$row->arrears}}</td>
-    
-    @foreach (json_decode($row->charges_breakup) as $ck => $crow )
-          @foreach ($charges_types as $ctkey => $ctrow )
-            @if($ctrow->title==$crow->charges_type)
-              <td class="">{{$crow->charges_type.' / '.$crow->charges}}</td>
+    {{--  changes breakup start here  --}}
+    @php 
+        $charges_breakup=json_decode($row->charges_breakup);
+        $qtrta=0;
+        $fssurcharge=0;
+        // $l.psurcharge=0;
+    @endphp
+    @foreach ($charges_breakup as $ck => $crow )
+            @if($crow->code=='QTRTA')
+            @php $qtrta=$crow->calculated_charges; @endphp
             @endif
-          @endforeach
+
+            @if($crow->charges_type=='F.S Surcharge')
+            @php $fssurcharge=$crow->calculated_charges; @endphp
+            @endif
     @endforeach
+          
+          <td class="">{{$qtrta}}</td>
+          <td class="">{{$fssurcharge}}</td>
 
-    @foreach (json_decode($row->taxes_breakup) as $tk => $trow )
-    <td class="">{{$trow->tax_type.' / '.$trow->calculated_tax}}</td>
+    {{-- changes breakup end  --}}
+    {{-- taxes breakup start --}}
+    @php 
+      $tax_breakup=json_decode($row->taxes_breakup);
+       $fpa=0;
+       $ed=0;
+       $edfpa=0;
+    @endphp
+    
+    @foreach ( $tax_breakup as $tk => $trow )
+           @if($trow->code=='FPA')
+               @php $fpa= $trow->calculated_tax @endphp
+            @endif
+
+            @if($trow->code=='ED')
+              @php $ed= $trow->calculated_tax; @endphp
+            @endif
+
+            @if($trow->code=='EDFPA')
+                 @php $edfpa=$trow->calculated_tax; @endphp
+            @endif
+   
     @endforeach
-
-       
-
+    
+    <td class="">{{$fpa}}</td>
+    <td class="">{{$ed}}</td>
+    <td class="">{{$edfpa}}</td>
+    {{-- taxes breakup end  --}}
       
     {{-- <td class="">{{$row->net_bill}}</td> --}}
    
-    {{-- <td class="">{{$row->WithinDuedate}}</td> --}}
-    {{-- <td class="">{{$row->l_p_surcharge}}</td> --}}
+    <td class="">{{$row->service_charges}}</td>
+    <td class="">{{$row->l_p_surcharge}}</td>
+    <td class="">{{$row->adjustment}}</td>
     <td class="">{{$row->AfterdueDate}}</td>
   </tr>
   <?php $c++;  $n_b_total+=$row->net_bill;$l_p_total=$row->l_p_surcharge;$a_total=$row->arrears; $wd_total=$row->WithinDuedate;  $ad_total=$row->AfterdueDate; ?>
